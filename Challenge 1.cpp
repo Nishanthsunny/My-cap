82a07959376820728f1f0aab64c2a8b541fd13c9
Nishanthsunny <67831240+Nishanthsunny@users.noreply.github.com>
2020-07-21 15:06:38 +0530

    C++ program to find size of datatypes

---
+++ Challenge 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a;
+	float b;
+	char c;
+	double d;
+	cout<<"Size of char: "<<sizeof(c)<< " byte\n";
+	cout<<"Size of int: "<<sizeof(b)<< " bytes\n";
+	cout<<"Size of float: "<<sizeof(a)<< " bytes\n";
+	cout<<"Size of double: "<<sizeof(d)<< " bytes\n";
+	
+	return 0;
+}
