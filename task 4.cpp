60d2b82841a0d1cad92189a029de81ce0d59ba9f
Nishanthsunny <67831240+Nishanthsunny@users.noreply.github.com>
2020-07-21 15:46:08 +0530

    To enter elements of array

---
+++ task 4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i,n;
+	cout<<"Enter the number of elements you want to enter ";
+	cin>>n;
+	int a[n];
+	cout<<"\nEnter elements : ";
+	for(i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	cout<<"\nYou entered : ";
+	for(i=0;i<n;i++)
+	{
+		cout<<a[i]<<endl;
+	}
+}
